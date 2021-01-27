c8b1c2e15d81a6bfdc5cda4e51e1416f828aac0d
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2021-01-27 11:20:15 -0300

    what are we gonna test?

--- tests/edgeset/test_edgeset.py
+++ tests/edgeset/test_edgeset.py
@@ -10,6 +10,90 @@ class TestEdgeSet(unittest.TestCase):
         edset = EdgeSet()
         self.assertFalse(len(edset))
 
+    def test_init_with_list_of_tuples(self):
+        raise NotImplementedError()
+
+    def test_init_with_list_of_edges(self):
+        raise NotImplementedError()
+
+    def test_init_with_tuple_of_tuples(self):
+        raise NotImplementedError()
+
+    def test_init_with_tuple_of_edges(self):
+        raise NotImplementedError()
+
+    def test_init_with_set_of_tuples(self):
+        raise NotImplementedError()
+
+    def test_init_with_set_of_edges(self):
+        raise NotImplementedError()
+
+    def init_with_edgeset(self):
+        raise NotImplementedError()
+
+    def test_contains(self):
+        raise NotImplementedError()
+
+    def test_if_is_iterable(self):
+        raise NotImplementedError()
+
+    def test_len_property(self):
+        raise NotImplementedError()
+
+    def test_sub_operand(self):
+        raise NotImplementedError()
+
+    def test_and_operand(self):
+        raise NotImplementedError()
+
+    def test_xor_operand(self):
+        raise NotImplementedError()
+
+    def test_or_operand(self):
+        raise NotImplementedError()
+
+    def test_eq_operand(self):
+        raise NotImplementedError()
+
+    def test_not_equal_operand(self):
+        raise NotImplementedError()
+
+    def test_less_than_operand(self):
+        raise NotImplementedError()
+
+    def test_greater_than_operand(self):
+        raise NotImplementedError()
+
+    def test_greater_or_equal_opearand(self):
+        raise NotImplementedError()
+
+    def test_less_or_equal_operand(self):
+        raise NotImplementedError()
+
+    def test_vertices_property(self):
+        raise NotImplementedError()
+
+    def test_add_method(self):
+        raise NotImplementedError()
+
+    def test_discard_method(self):
+        raise NotImplementedError()
+
+    def test_remove_method(self):
+        raise NotImplementedError()
+
+    def test_issubset_method(self):
+        raise NotImplementedError()
+
+    def test_issuperset_method(self):
+        raise NotImplementedError()
+
+    def test_clear_method(self):
+        raise NotImplementedError()
+
+    def test_copy_method(self):
+        raise NotImplementedError()
+
 
 if __name__ == '__main__':
     unittest.main()

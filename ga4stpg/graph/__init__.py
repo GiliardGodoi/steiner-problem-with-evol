77a40e4db92825c837d1621b0609ca8ef4cf3c40
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-01-28 01:00:25 -0300

    change __all__ property

--- ga4stpg/graph/__init__.py
+++ ga4stpg/graph/__init__.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from .reader import SteinerTreeProblem, Reader, ReaderORLibrary
 from .graph import UndirectedWeightedGraph as Graph
+from .graph import UndirectedGraph
+from .graph import UndirectedGraph as UGraph
 
 __all__ = [
     "Graph",
+    "UndirectedGraph",
+    "UGraph",
     "Reader",
     "ReaderORLibrary",
     "SteinerTreeProblem"

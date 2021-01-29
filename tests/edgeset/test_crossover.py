3b9f0cff476d9fafcb189517cbb7a92e6dea8192
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2021-01-28 21:14:34 -0300

    implemented TestCrossoverPrimRSTBased

--- tests/edgeset/test_crossover.py
+++ tests/edgeset/test_crossover.py
@@ -12,6 +12,66 @@ from ga4stpg.graph import ReaderORLibrary, UGraph
 from ga4stpg.graph.util import is_steiner_tree
 
 
+class TestCrossoverPrimRSTBased(unittest.TestCase):
+
+    def test_if_return_an_edgeset(self):
+        filename = path.join('datasets', 'ORLibrary', 'steinb15.txt')
+        stpg  = ReaderORLibrary().parser(filename)
+
+        crossover = CrossoverPrimRST(stpg)
+
+        parent_a = gen_random_walk(stpg)
+        parent_b = gen_random_walk(stpg)
+
+        self.assertIsInstance(parent_a, EdgeSet)
+        self.assertIsInstance(parent_b, EdgeSet)
+
+        offspring = crossover(parent_a, parent_b)
+
+        self.assertIsInstance(offspring, EdgeSet)
+        self.assertFalse(offspring is parent_a)
+        self.assertFalse(offspring is parent_b)
+
+        self.assertNotEqual(offspring, parent_a)
+        self.assertNotEqual(offspring, parent_b)
+
+    def test_if_has_all_terminals(self):
+        filename = path.join('datasets', 'ORLibrary', 'steinb15.txt')
+        stpg  = ReaderORLibrary().parser(filename)
+        terminals = stpg.terminals
+
+        crossover = CrossoverPrimRST(stpg)
+
+        parent_a = gen_random_walk(stpg)
+        parent_b = gen_random_walk(stpg)
+        offspring = crossover(parent_a, parent_b)
+
+        vertices = set([v for v in offspring.vertices])
+        self.assertTrue(terminals.issubset(vertices))
+
+    def test_if_is_it_a_tree(self):
+        filename = path.join('datasets', 'ORLibrary', 'steinb15.txt')
+        stpg  = ReaderORLibrary().parser(filename)
+
+        crossover = CrossoverPrimRST(stpg)
+
+        parent_a = gen_random_walk(stpg)
+        parent_b = gen_random_walk(stpg)
+        offspring = crossover(parent_a, parent_b)
+
+        tree = UGraph()
+        for edge in offspring:
+            u, v = edge[0], edge[1]
+            tree.add_edge(u, v)
+
+        _, response = is_steiner_tree(tree, stpg)
+        self.assertTrue(response['all_terminals_in'])
+        self.assertFalse(response['has_cycle'])
+        self.assertTrue(response['all_edges_are_reliable'])
+        self.assertTrue(response['graph_is_connected'])
+        # It is not possible to grant that 'all_leaves_are_terminals'
+
+
 class TestCrossoverRandomWalkBased(unittest.TestCase):
 
     def test_if_return_an_edgeset(self):
